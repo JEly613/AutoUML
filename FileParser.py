b828036fee9e8229fbee353a371a35302d262cf9
JEly613 <justinyely@gmail.com>
2026-03-16 15:29:05 -0700

    FileParser.printClass complete

--- FileParser.py
+++ FileParser.py
@@ -4,9 +4,24 @@ import re
 class FileParser:
     def __init__(self, filePath):
         self.className = _readValues()['name']
-        self.classVars = _readValues['vars']
-        self.classMethods = _readValues['methods']
+        self.classVars = _readValues()['vars']
+        self.classMethods = _readValues()('methods')
 
+def printClass():
+    # print class header
+    print("class ", self.className, " {")
+
+    # print instance variables
+    for var in self.classVars:
+        print(str(var))
+
+    # print methods
+    for method in self.classMethods:
+        print(str(method))
+
+    # closing bracket
+    print("}")
+    
 '''
 TODO:
 printClass, prints class in plantuml format
